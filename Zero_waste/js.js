60b6e34a03577c0079c95ac963af790e9e55c760
Rohit kumar <144412423+rohit99-23@users.noreply.github.com>
2025-04-11 09:12:31 +0530

    Delete Zero_waste directory

--- Zero_waste/js.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("signupForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Signup successful!");
-  });
-  function selectRole(role){
-    window.location.href = "sigin.html";
-  }
